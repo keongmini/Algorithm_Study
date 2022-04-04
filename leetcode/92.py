1cf84cb151e08791db52aa1f0795b793b6ef170c
keongmin <kmnam09@gmail.com>
2022-04-04 11:57:07 +0900

    Reverse Linked List

---
+++ leetcode/92.py
@@ -0,0 +1,25 @@
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution(object):
+    def reverseBetween(self, head, left, right):
+
+        if not head or left == right:
+            return head
+
+        root = start = ListNode(None)
+        root.next = head
+
+        for _ in range(left - 1):
+            start = start.next
+        end = start.next
+
+        for _ in range(right - left):
+            tmp = start.next
+            start.next = end.next
+            end.next = end.next.next
+            start.next.next = tmp
+
+        return root.next
