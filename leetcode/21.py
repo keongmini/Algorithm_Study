d75093b8c6ce1f0e30e7fb1a8d4004cf46d7014a
keongmin <kmnam09@gmail.com>
2022-04-18 23:20:34 +0900

    Merge Two Sorted Lists(recursive)

--- leetcode/21.py
+++ leetcode/21.py
@@ -59,3 +59,14 @@ class Solution:
         else:
             l2.next = self.mergeTwoLists(l1, l2.next)
             return l2
+
+# 재귀 풀이
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        if (not list1) or (list2 and list1.val > list2.val):
+            list1, list2 = list2, list1
+
+        if list1:
+            list1.next = self.mergeTwoLists(list1.next, list2)
+
+        return list1
