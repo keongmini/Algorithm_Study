3543293ffff453eec9cd3fa2d3942fcbb18d8c01
keongmin <kmnam09@gmail.com>
2022-04-18 23:19:41 +0900

    Reverse Linked List(recursive)

--- leetcode/206.py
+++ leetcode/206.py
@@ -17,3 +17,14 @@ ListNode{val: 3, next: ListNode{val: 2, next: ListNode{val: 1, next: None}}}
 ListNode{val: 4, next: ListNode{val: 3, next: ListNode{val: 2, next: ListNode{val: 1, next: None}}}}
 ListNode{val: 5, next: ListNode{val: 4, next: ListNode{val: 3, next: ListNode{val: 2, next: ListNode{val: 1, next: None}}}}}
 '''
+
+# 재귀 풀이
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        def reverse(node: ListNode, prev: ListNode = None):
+            if not node:
+                return prev
+            next, node.next = node.next, prev
+            return reverse(next, node)
+
+        return reverse(head)
