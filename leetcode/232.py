16207e2cd935ee116a38cf9f78077fe677ddde26
keongmin <kmnam09@gmail.com>
2022-04-23 16:59:20 +0900

    Implement Queue using Stack

--- leetcode/232.py
+++ leetcode/232.py
@@ -15,6 +15,9 @@ class MyQueue(object):
             while self.input:
                 self.output.append(self.input.pop())
         return self.output[-1]
+        # 큐를 구현 -> 큐는 앞에 있는 값부터 빼야 함
+        # 뒤집게 되면 맨 앞에 있는 값이 맨 뒤로 가게 됨 -> 스택처럼 뒤에서 값을 빼도 값은 큐처럼 앞에 있는 값이 빠지는 것처럼 출력
+        # 계속 뒤에서 값이 들어오고 나가는 건 앞에서 나가는 거니까 output에 값이 다 없어질 때까지 업데이트 해주지 않아도됨
 
     def empty(self):
         return self.input == [] and self.output == []
