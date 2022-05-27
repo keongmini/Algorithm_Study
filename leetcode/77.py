65be419281b849bffc982a124c395e96cbe644fa
keongmin <kmnam09@gmail.com>
2022-05-28 00:03:53 +0900

    Combinations

--- leetcode/77.py
+++ leetcode/77.py
@@ -27,3 +27,20 @@ class Solution:
 
         dfs([], 1, k)
         return result
+
+# dfs 풀이 더 효율적으로 풀기
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        result = []
+
+        def dfs(elements, index):
+            if len(elements) == k:
+                result.append(elements[:])
+
+            for i in range(index, n + 1):
+                elements.append(i)
+                dfs(elements, i + 1)
+                elements.pop()
+
+        dfs([], 1)
+        return result
