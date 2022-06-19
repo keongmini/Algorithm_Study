c568ed0ee640153f0be03f576056bce14ecf1069
keongmin <kmnam09@gmail.com>
2022-06-19 23:17:07 +0900

    타겟 넘버

---
+++ programmers/43165.py
@@ -0,0 +1,14 @@
+def solution(numbers, target):
+    cnt = []
+
+    def dfs(num, result):
+        if len(numbers) == num:
+            if result == target:
+                cnt.append(1)
+            return
+
+        dfs(num + 1, result + numbers[num])
+        dfs(num + 1, result - numbers[num])
+
+    dfs(0, 0)
+    return len(cnt)
