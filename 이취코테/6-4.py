b1baa0a2d878077be8b58d3699c535e9e3c58766
kkminii <103486036+kkminii@users.noreply.github.com>
2022-08-04 11:42:38 +0900

    두 배열의 원소 교체

---
+++ 이취코테/6-4.py
@@ -0,0 +1,15 @@
+n, k = map(int, input().split())
+
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+
+A.sort()
+B.sort(reverse=True)
+
+for i in range(k):
+    if A[i] < B[i]:
+        A[i], B[i] = B[i], A[i]
+    else:       # 더이상 진행할 필요x
+        break
+
+print(sum(A))
