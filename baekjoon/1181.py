09fe3c1314bb6877d21827351fdd71d8f26be203
kkminii <103486036+kkminii@users.noreply.github.com>
2022-08-04 12:09:55 +0900

    단어 정렬

---
+++ baekjoon/1181.py
@@ -0,0 +1,37 @@
+# 삽입 정렬 - 시간초과
+n = int(input())
+
+words = []
+for _ in range(n):
+    words.append(input())
+
+words = list(set(words))
+
+for i in range(len(words)):
+    for j in range(i, 0, -1):
+        if len(words[j]) < len(words[j - 1]):
+            words[j], words[j - 1] = words[j - 1], words[j]
+        elif len(words[j]) == len(words[j - 1]) and words[j] < words[j - 1]:
+            words[j], words[j - 1] = words[j - 1], words[j]
+        else:
+            break
+
+for word in words:
+    print(word)
+
+# 정렬 라이브러리 사용 - 통과
+n = int(input())
+
+words = []
+for _ in range(n):
+    words.append(input())
+words = list(set(words))
+
+word_list = []
+for word in words:
+    word_list.append([len(word), word])
+
+word_list.sort()
+
+for word in word_list:
+    print(word[1])
