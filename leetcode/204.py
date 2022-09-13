84da2b493e2376240fc56ddd932f68f35b74a88a
keongmini <kmnam09@gmail.com>
2022-09-13 16:47:53 +0900

    Count Primes

---
+++ leetcode/204.py
@@ -0,0 +1,13 @@
+class Solution:
+    def countPrimes(self, n: int) -> int:
+        if n <= 2:
+            return 0
+
+        nums = [False, False] + [True] * (n - 2)
+
+        for i in range(2, int(sqrt(n)) + 1):
+            if nums[i]:
+                for p in range(i * i, n, i):
+                    nums[p] = False
+
+        return sum(nums)
