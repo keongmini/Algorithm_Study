ac1cacd3c9d1d440bda5da6343b40b034a29093b
keongmini <kmnam09@gmail.com>
2022-09-17 14:08:14 +0900

    Fibonacci Number

---
+++ leetcode/509.py
@@ -0,0 +1,10 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        if n == 0:
+            return 0
+        nums = [0] * (n + 1)
+        nums[1] = 1
+        for i in range(2, n + 1):
+            nums[i] = nums[i - 1] + nums[i - 2]
+
+        return nums[n]
