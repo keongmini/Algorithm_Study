fda1b68da0f44d48eb6afe2949644f62b8a05b8a
keongmini <kmnam09@gmail.com>
2022-10-03 14:11:28 +0900

    BinaryGap

---
+++ codility/iterations.py
@@ -0,0 +1,15 @@
+def solution(N):
+    bin = format(N, 'b')
+
+    bin = str(int(bin))
+
+    if bin.count('1') <= 1 or bin.count('1') == len(bin):
+        return 0
+
+    idx = [i for i in range(len(bin)) if bin[i] == '1']
+
+    result = 0
+    for i in range(len(idx) - 1):
+        result = max(idx[i + 1] - idx[i], result)
+
+    return result - 1
