4620dcccd0222572971c9539a22942c5ac4c15b1
keongmini <kmnam09@gmail.com>
2022-10-03 14:28:51 +0900

    OddOccurrencesInArray

---
+++ codility/Arrays2.py
@@ -0,0 +1,18 @@
+def solution(A):
+    A.sort()
+
+    now = A[0]
+    cnt = 1
+    for i in range(1, len(A)):
+        if A[i] == now:
+            cnt += 1
+        else:
+            if not cnt % 2:
+                now = A[i]
+                cnt = 1
+            else:
+                return now
+
+    return now
+
+# 시간 복잡도 O(N) or O(N*log(N))
