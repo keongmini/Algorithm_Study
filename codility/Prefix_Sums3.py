6c5d24c1caf83ed8330e976d3dcc916497feaff0
keongmini <kmnam09@gmail.com>
2022-10-05 17:29:31 +0900

    GenomicRangeQuery

--- codility/Prefix_Sums3.py
+++ codility/Prefix_Sums3.py
@@ -43,3 +43,65 @@ def solution(S, P, Q):
                 break
 
     return result
+
+
+def solution(S, P, Q):
+    N = {
+        "A": 1,
+        "C": 2,
+        "G": 3,
+        "T": 4
+    }
+
+    result = []
+    for i in range(len(P)):
+        minNum = 4
+        nums = set(S[P[i]: Q[i] + 1])
+        for n in nums:
+           minNum = min(minNum, N[n])
+        result.append(minNum)
+
+    return result
+
+# O(N + M)
+def solution(S, P, Q):
+    N = {
+        "A": 1,
+        "C": 2,
+        "G": 3,
+        "T": 4
+    }
+
+    result = []
+    for i in range(len(P)):
+        char = S[P[i]: Q[i] + 1]
+        for s in N:
+            if s in char:
+                result.append(N[s])
+                break
+    return result
+
+
+def solution(S, P, Q):
+    N = {
+        "A": 1,
+        "C": 2,
+        "G": 3,
+        "T": 4
+    }
+
+    result = []
+    for i in range(len(P)):
+        char = S[P[i]: Q[i] + 1]
+        if 'A' in char:
+            num = 1
+        elif 'C' in char:
+            num = 2
+        elif 'G' in char:
+            num = 3
+        elif 'T' in char:
+            num = 4
+
+        result.append(num)
+
+    return result
