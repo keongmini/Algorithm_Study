a446893d164020503bd351323bfa18cc89e3e4f3
keongmini <kmnam09@gmail.com>
2022-10-25 12:34:54 +0900

    Maximum Product Subarray

---
+++ leetcode/152.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        if len(nums) == 0:
+            return 0
+
+        max_so_far = nums[0]
+        min_so_far = nums[0]
+        result = max_so_far
+
+        for i in range(1, len(nums)):
+            curr = nums[i]
+            temp_max = max(curr, max_so_far * curr, min_so_far * curr)
+            min_so_far = min(curr, max_so_far * curr, min_so_far * curr)
+
+            max_so_far = temp_max       # min_so_far 구하는 부분에서 바뀌기 전 max_so_far값 사용하기 위해
+
+            result = max(max_so_far, result)
+
+        return result
