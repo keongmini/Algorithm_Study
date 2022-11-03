bd6019b704ebd338529db61836eb4670f81b9fa0
keongmini <kmnam09@gmail.com>
2022-11-03 17:33:40 +0900

    Diameter of Binary Tree

--- leetcode/543.py
+++ leetcode/543.py
@@ -15,8 +15,31 @@ class Solution:
             left = dfs(node.left)
             right = dfs(node.right)
 
-            self.longest = max(self.longest, left + right + 2)
+            self.longest = max(self.longest, left + right + 2)      # root에서 left로 연결되는 부분, right로 연결되는 부분 2개
             return max(left, right) + 1
 
         dfs(root)
         return self.longest
+
+
+class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        maxLength = 0
+
+        def dfs(node):
+            nonlocal maxLength
+
+            if not node:
+                return 0
+
+            leftLength = dfs(node.left)
+            rightLength = dfs(node.right)
+
+            maxLength = max(maxLength, leftLength + rightLength)
+
+            return max(leftLength, rightLength) + 1
+
+        dfs(root)
+        return maxLength
+
+# 풀이는 길이의 영역을 어디까지 보는지에 따라 달라짐
