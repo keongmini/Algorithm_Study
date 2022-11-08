8a21e48ce6c7ebc287ddc3347e6e7b34fcc0e09c
keongmini <kmnam09@gmail.com>
2022-11-08 19:28:18 +0900

    최소 신장 트리

--- baekjoon/1197.py
+++ baekjoon/1197.py
@@ -4,10 +4,11 @@ q = []
 
 for _ in range(m):
     a, b, w = map(int, input().split())
-    heapq.heappush(q, (w, a, b))
+    heapq.heappush(q, (w, a, b))        # 가중치를 기준으로 정렬
 
 parent = [i for i in range(n + 1)]
 
+# 유니온 파인드 구현 - 해당 문제에서 파인드 제외
 def getParent(parent, x):
     if parent[x] == x:
         return x
@@ -27,8 +28,8 @@ result = 0
 while q:
     w, a, b = heapq.heappop(q)
 
-    if getParent(parent, a) != getParent(parent, b):
-        unionParent(parent, a, b)
+    if getParent(parent, a) != getParent(parent, b):        #  부모 노드 찾기 / 같을 경우 사이클
+        unionParent(parent, a, b)       # Union
         result += w
 
 print(result)
