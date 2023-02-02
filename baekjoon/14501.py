5321225956fc4e0d9365d7854033cca94f9a71c4
keongmini <kmnam09@gmail.com>
2023-02-02 16:13:33 +0900

    퇴사

---
+++ baekjoon/14501.py
@@ -0,0 +1,18 @@
+N = int(input())
+
+T = []
+P = []
+for _ in range(N):
+    n, m = map(int, input().split())
+    T.append(n)
+    P.append(m)
+
+# ----------------------- 입력값 정리
+
+dp = [0 for _ in range(N + 1)]
+
+for i in range(N):
+    for j in range(i + T[i], N + 1):
+        dp[j] = max(dp[j], dp[i] + P[i])
+
+print(dp[-1])
