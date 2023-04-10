f7a7ddc4951f938e8919d3de9af8797f00b2ee07
keongmini <kmnam09@gmail.com>
2023-04-10 17:51:48 +0900

    3의개수

---
+++ cpp/29.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n, tmp, i, cnt=0, digit;
+    cin>>n;
+
+    for(i=1; i<=n; i++){
+        tmp=i;
+        while(tmp>0){
+            digit=tmp%10;
+            if(digit==3) cnt++;
+            tmp = tmp / 10;
+        }
+    }
+
+    cout<<cnt;
+
+    return 0;
+}
