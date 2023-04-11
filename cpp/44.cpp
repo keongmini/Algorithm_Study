bf4c70b6c32ed4b3a27c9071191791c3f4faf3aa
keongmini <kmnam09@gmail.com>
2023-04-11 20:56:09 +0900

    마구간 정하기

---
+++ cpp/44.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int n;
+
+int Count(int len, int x[]){
+    int i, cnt=1, pos=x[1];
+    for(i=2;i<=n;i++){
+        if(x[i] - pos >= len){
+            cnt++;
+            pos=x[i];
+        }
+    }
+    return cnt;
+}
+
+int main(){
+    int c, i, left=0, right, mid, result;
+    cin>>n>>c;
+    int *location = new int[n+1];  // 제약조건이 클 때, 동적으로 배열 설정
+
+    for(i=1;i<=n;i++){
+        cin>>location[i];
+    }
+
+    sort(location+1, location+n+1);   // vector가 아닌 배열 정렬
+    right = location[n];
+
+    while(left<=right){
+        mid = (left + right) / 2;
+        if(Count(mid, location) >= c){
+            result = mid;
+            left = mid + 1;
+        }
+        else right = mid - 1;
+    }
+    cout<<result;
+
+    return 0;
+}
