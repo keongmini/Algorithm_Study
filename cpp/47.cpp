7832ab13af7f2e7f0da821b604f171093aeb0b1b
keongmini <kmnam09@gmail.com>
2023-04-11 21:42:50 +0900

    봉우리(vector로 2차원 배열 선언)

--- cpp/47.cpp
+++ cpp/47.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
+#include<vector>
 using namespace std;
-int a[51][51]; // 2차원 배열
+// int a[60][60]; // 2차원 배열
 
 // 상하좌우
 int dx[4] = {-1, 0, 1, 0};
@@ -9,6 +10,9 @@ int dy[4] = {0, -1, 0, 1};
 int main(){
     int n, i, j, k, cnt=0, flag;
     cin>>n;
+
+    vector<vector<int> > a(n+2, vector<int>(n+2,0));        // vector로 2차원 배열 선언
+
     for(i=1;i<=n;i++){
         for(j=1;j<=n;j++){
             cin>>a[i][j];
