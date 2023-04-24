f1986d67a6eb6e5eb96a522fa93916c8504f7108
keongmini <kmnam09@gmail.com>
2023-04-24 23:52:21 +0900

    문자열 다루기 기본

---
+++ programmers/12918.py
@@ -0,0 +1,9 @@
+def solution(s):
+    if len(s) == 4 or len(s) == 6:
+        for char in s:
+            if not char.isdigit():
+                return False
+    else:
+        return False
+
+    return True
