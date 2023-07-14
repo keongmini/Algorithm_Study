ebc66687a33210dddb4310101d79357af9238887
keongmini <kmnam09@gmail.com>
2023-07-14 13:58:53 +0900

    Number of Islands

--- leetcode/200.py
+++ leetcode/200.py
@@ -1,3 +1,4 @@
+# dfs
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         def dfs(i, j):
@@ -20,3 +21,46 @@ class Solution:
                     count += 1
 
         return count
+
+
+from collections import deque
+
+
+# bfs
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        move = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        N = len(grid)
+        M = len(grid[0])
+
+        visited = [[False for _ in range(M)] for i in range(N)]
+
+        def bfs(x, y):
+            q = deque()
+            q.append((x, y))
+            visited[x][y] = True
+
+            while q:
+                a, b = q.popleft()
+
+                for i, j in move:
+                    na = a + i
+                    nb = b + j
+
+                    if na < 0 or na >= N or nb < 0 or nb >= M:
+                        continue
+
+                    if grid[na][nb] == "0" or visited[na][nb]:
+                        continue
+
+                    visited[na][nb] = True
+                    q.append((na, nb))
+
+        result = 0
+        for i in range(N):
+            for j in range(M):
+                if not visited[i][j] and grid[i][j] == "1":
+                    bfs(i, j)
+                    result += 1
+
+        return result
