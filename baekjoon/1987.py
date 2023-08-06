daca9aa7acd1f83f1d3581a0569fd115e03b1d0f
keongmini <kmnam09@gmail.com>
2023-08-06 18:24:30 +0900

    알파벳 - 시간초과 해결

--- baekjoon/1987.py
+++ baekjoon/1987.py
@@ -1,4 +1,4 @@
-# 시간 초과
+# bfs, set - 통과
 import sys
 
 input = sys.stdin.readline
@@ -7,32 +7,73 @@ R, C = map(int, input().split())
 
 graph = [[s for s in input()] for _ in range(R)]
 
-visited = {}
-
-result = 1
-
 move = [(1, 0), (0, 1), (-1, 0), (0, -1)]
 
 
-def dfs(x, y, cnt):
-    global result
+def bfs(x, y):
+    result = 1
 
-    result = max(result, cnt)
+    s = set()
+    s.add((x, y, 1, graph[x][y]))
 
-    visited[graph[x][y]] = True
+    while s:
+        i, j, cnt, passed = s.pop()         # passed = 문자열 - 문자열에 계속 누적해서 값 저장
 
-    for a, b in move:
-        nx = x + a
-        ny = y + b
+        for a, b in move:
+            nx = i + a
+            ny = j + b
 
-        if nx < 0 or nx >= R or ny < 0 or ny >= C or graph[nx][ny] in visited:
-            continue
+            if nx < 0 or nx >= R or ny < 0 or ny >= C:
+                continue
 
-        dfs(nx, ny, cnt + 1)
+            if graph[nx][ny] in passed:
+                continue
 
-    del visited[graph[x][y]]
+            s.add((nx, ny, cnt + 1, passed + graph[nx][ny]))
+
+        result = max(result, cnt)
+
+    return result
 
 
-dfs(0, 0, 1)
+print(bfs(0, 0))
 
-print(result)
+
+# dfs, 딕셔너리 - 시간 초과
+# import sys
+#
+# input = sys.stdin.readline
+#
+# R, C = map(int, input().split())
+#
+# graph = [[s for s in input()] for _ in range(R)]
+#
+# visited = {}
+#
+# result = 1
+#
+# move = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+#
+#
+# def dfs(x, y, cnt):
+#     global result
+#
+#     result = max(result, cnt)
+#
+#     visited[graph[x][y]] = True
+#
+#     for a, b in move:
+#         nx = x + a
+#         ny = y + b
+#
+#         if nx < 0 or nx >= R or ny < 0 or ny >= C or graph[nx][ny] in visited:
+#             continue
+#
+#         dfs(nx, ny, cnt + 1)
+#
+#     del visited[graph[x][y]]
+#
+#
+# dfs(0, 0, 1)
+#
+# print(result)
