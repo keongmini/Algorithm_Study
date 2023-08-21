3e3d737ca8fc55cdbcb28d22546b79f6eb23f27f
keongmini <kmnam09@gmail.com>
2023-08-21 20:15:25 +0900

    동전 2

--- baekjoon/2294.py
+++ baekjoon/2294.py
@@ -4,7 +4,7 @@ input = sys.stdin.readline
 
 n, k = map(int, input().split())
 
-coin = set()
+coin = set()            # 중복 제거
 
 for _ in range(n):
     tmp = int(input())
@@ -12,7 +12,7 @@ for _ in range(n):
         coin.add(tmp)
 
 coin = list(coin)
-coin.sort()
+coin.sort()             # 작은 수 부터 확인
 
 dp = [1e9 if i not in coin else 1 for i in range(k + 1)]
 
@@ -20,10 +20,10 @@ for i in range(1, k + 1):
     for c in coin:
         now = i - c
 
-        if now <= 0:
+        if now <= 0:            # 1부터 시작이니까
             break
 
-        dp[i] = min(dp[i], dp[now] + 1)
+        dp[i] = min(dp[i], dp[now] + 1)         # 현재 값과 이전 값 + 1 비교
 
 if dp[k] == 1e9:
     print(-1)
