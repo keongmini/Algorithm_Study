bf26829c87f8381cd1c6ad7c5cf6dad5d589a8c3
keongmini <kmnam09@gmail.com>
2023-09-14 07:41:08 +0900

    유기농 배추

--- baekjoon/1012.py
+++ baekjoon/1012.py
@@ -7,6 +7,28 @@ T = int(input())
 
 move = [(1, 0), (-1, 0), (0, 1), (0, -1)]
 
+
+def bfs(x, y):
+    q = deque()
+    q.append((x, y))
+
+    while q:
+        a, b = q.popleft()
+
+        for u, v in move:
+            na = a + u
+            nb = b + v
+
+            if na < 0 or na >= M or nb < 0 or nb >= N:
+                continue
+
+            if graph[na][nb] != 1:
+                continue
+
+            q.append((na, nb))
+            graph[na][nb] = -1
+
+
 for _ in range(T):
     M, N, K = map(int, input().split())
 
@@ -15,28 +37,6 @@ for _ in range(T):
     for k in range(K):
         x, y = map(int, input().split())
         graph[x][y] = 1
-
-
-    def bfs(x, y):
-        q = deque()
-        q.append((x, y))
-
-        while q:
-            a, b = q.popleft()
-
-            for u, v in move:
-                na = a + u
-                nb = b + v
-
-                if na < 0 or na >= M or nb < 0 or nb >= N:
-                    continue
-
-                if graph[na][nb] != 1:
-                    continue
-
-                q.append((na, nb))
-                graph[na][nb] = -1
-
 
     result = 0
     for i in range(M):
